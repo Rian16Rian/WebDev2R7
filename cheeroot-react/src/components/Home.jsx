83b558b44e16a46cf8d84e5d91699caa93644954
Kristal Angeline Sarsoza <164842102+MissKaida03@users.noreply.github.com>
2024-06-22 20:11:33 +0800

    fix explore button

--- cheeroot-react/src/components/Home.jsx
+++ cheeroot-react/src/components/Home.jsx
@@ -1,23 +1,30 @@
+
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 function Home() {
+  const scrollToTitle = () => {
+    const element = document.getElementById('titlecontainer');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <>
     <header className="home">
       <div className="overlay">
         <h1>FRESH HERBS</h1>
         <h2>Natural & Nutritious</h2>
         <p>Elevate Your Journey With Fresh Flavors And Timeless Traditions</p>
-        <a href="#titlecontainer" className="explore-button">EXPLORE</a>
+        <Link to="#titlecontainer" className="explore-button" onClick={scrollToTitle}>EXPLORE</Link>
         <div className="icon">
           <FontAwesomeIcon icon={faAngleDown} />
         </div>
       </div>
     </header>
-  </>
-);
+  );
 }
 
 export default Home;
